b46448f8368a7dbe4e8eb790e833d831a7e9c4a8
krola1 <kwl082024@gmail.com>
2025-09-24 15:04:24 +0200

    Moved props and parameters to objects in app,  structure and comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import { useState } from "react";
 import { useLocalStorage } from "./hooks/useLocalStorage.js";
 import "./App.css";
-
 import TaskList from "./components/TaskList.jsx";
 import Toolbar from "./components/Toolbar/Toolbar.jsx";
 
+//The main entry poinyt for out app, contains global states, the main array and functions related to editing the array.
+//
+
 function App() {
+  //--------------------------states--------------------------
   const [list, setList] = useLocalStorage("list", []);
   const [input, setInput] = useState("");
   const [showCompleted, setShowCompleted] = useState(true);
   const [sortMode, setSortMode] = useState("newFirst");
   const [query, setQuery] = useState("");
 
+  //--------------------------List editing functions --------------------------
   // adds task object to list
   const handleAdd = () => {
     if (input.trim() !== "") {
@@ -44,38 +48,46 @@ function App() {
     );
   };
 
+  // --------------------------Objects for passing props--------------------------
+
+  //Toolbar Props
+  const toolbarProps = {
+    setShowCompleted,
+    showCompleted,
+    query,
+    setQuery,
+    sortMode,
+    setSortMode,
+  };
+  //TaskList Props
+  const taskListProps = {
+    sortMode,
+    showCompleted,
+    list,
+    onDelete: handleDelete,
+    onToggle: toggleComplete,
+    onEdit: editTask,
+    query,
+  };
+
+  //--------------------------parameters for other elements--------------------------
+  const inputParameters = {
+    type: "text",
+    value: input,
+    onChange: (e) => setInput(e.target.value),
+    onKeyDown: (e) => {
+      if (e.key === "Enter") {
+        handleAdd();
+      }
+    },
+  };
+  //--------------------------Rendering--------------------------
   return (
     <>
-      <input
-        type="text"
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-        onKeyDown={(e) => {
-          if (e.key === "Enter") {
-            handleAdd();
-          }
-        }}
-      />
-
+      <input {...inputParameters} />
       <button onClick={handleAdd}>add</button>
-
-      <Toolbar
-        setShowCompleted={setShowCompleted}
-        showCompleted={showCompleted}
-        query={query}
-        setQuery={setQuery}
-        sortMode={sortMode}
-        setSortMode={setSortMode}
-      />
-      <TaskList
-        sortMode={sortMode}
-        showCompleted={showCompleted}
-        list={list}
-        onDelete={handleDelete}
-        onToggle={toggleComplete}
-        onEdit={editTask}
-        query={query}
-      />
+      <Toolbar {...toolbarProps} />
+      <TaskList {...taskListProps} />
     </>
   );
 }
